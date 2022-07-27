b3bfff8e35865d732a0fa4752514e919de62f26c
rakibul-cse-diu <rakibul2180@gmail.com>
2022-07-27 13:58:20 +0600

    create layout and design

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log('script loded');
+
+const initCanvas = (id) => {
+    return new fabric.Canvas(id, {
+        width: 650,
+        height: 500,
+        backgroundColor: '#1B1B22'
+    });
+}
+
+const canvas = initCanvas('canvas');
+
+// const canvas = new fabric.Canvas('canvas', {
+//     width: 500,
+//     height: 500,
+//     backgroundColor: 'red',
+// });
+
+canvas.renderAll();
